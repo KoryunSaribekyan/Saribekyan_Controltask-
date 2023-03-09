13a72619c58907e1adb15f8c394854cf40ef68e8
Koryun <saribekyan.koryun@mail.ru>
2023-03-09 06:24:46 +0400

    Создаем  массив строк words размером n

--- controlTask/Program.cs
+++ controlTask/Program.cs
@@ -1,3 +1,7 @@
 ﻿Console.Clear();
 Console.Write("Введите количество слов в массиве: ");
 int n = int.Parse(Console.ReadLine());
+string[] words = new string[n];
+Console.WriteLine("Введите слова через пробел:");
+string input = Console.ReadLine();
+words = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
