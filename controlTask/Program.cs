f8e7da9dfd46bbc83d7b60cbd4d3390a79a085b8
Koryun <saribekyan.koryun@mail.ru>
2023-03-09 06:24:04 +0400

    Просим пользователя ввести  количество слов для массива

--- controlTask/Program.cs
+++ controlTask/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите количество слов в массиве: ");
+int n = int.Parse(Console.ReadLine());
