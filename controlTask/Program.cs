6cdbff904827bf6049cca0e2e56a77d14b3735ce
Koryun <saribekyan.koryun@mail.ru>
2023-03-09 06:26:01 +0400

    Выводим слова из массива words, длина которых не превышает 3 символа.

--- controlTask/Program.cs
+++ controlTask/Program.cs
@@ -5,3 +5,9 @@ string[] words = new string[n];
 Console.WriteLine("Введите слова через пробел:");
 string input = Console.ReadLine();
 words = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+for(int i = 0; i<words.Length; i++)
+{
+    if(words[i].Length<=3)
+    Console.Write($"{words[i]} ");
+
+}
